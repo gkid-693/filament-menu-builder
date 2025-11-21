2389474ff0fc10dd918b282a04bec81e80d35087
gkid <gervas.nyomboke@ega.go.tz>
2025-11-21 15:49:39 +0300

    updating packges to match filament v4

--- src/Resources/MenuResource/Pages/EditMenu.php
+++ src/Resources/MenuResource/Pages/EditMenu.php
@@ -5,35 +5,20 @@ declare(strict_types=1);
 namespace Datlechin\FilamentMenuBuilder\Resources\MenuResource\Pages;
 
 use Datlechin\FilamentMenuBuilder\Concerns\HasLocationAction;
-use Datlechin\FilamentMenuBuilder\FilamentMenuBuilderPlugin;
-use Filament\Actions;
-use Filament\Forms\Components\Section;
-use Filament\Forms\Form;
+use Datlechin\FilamentMenuBuilder\Resources\MenuResource;
+use Filament\Actions\DeleteAction;
 use Filament\Resources\Pages\EditRecord;
 
 class EditMenu extends EditRecord
 {
     use HasLocationAction;
 
-    protected static string $view = 'filament-menu-builder::edit-record';
-
-    public static function getResource(): string
-    {
-        return FilamentMenuBuilderPlugin::get()->getResource();
-    }
-
-    public function form(Form $form): Form
-    {
-        return $form
-            ->schema([
-                Section::make()->schema($form->getComponents()),
-            ]);
-    }
+    protected static string $resource = MenuResource::class;
 
     protected function getHeaderActions(): array
     {
         return [
-            Actions\DeleteAction::make(),
+            DeleteAction::make(),
             $this->getLocationAction(),
         ];
     }
